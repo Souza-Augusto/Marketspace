3dee311733f7ebf479887c59e1fe12cc8bc4d885
Augusto Souza <souzaaugusto164@gmail.com>
2023-08-08 14:22:01 -0300

    fix:bug styles typing fixed

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -4,9 +4,9 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  background-color: ${({theme}) => theme.COLORS.GRAY_200};
 `;
 
-export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
-  color: theme.COLORS.GREEN_DARK,
+export const LoadIndicator = styled.ActivityIndicator.attrs(({theme}) => ({
+  color: theme.COLORS.GRAY_200,
 }))``;
