97a844831e5933d556d5a7070047dbe9273a2e47
Augusto Souza <souzaaugusto164@gmail.com>
2023-08-11 09:48:12 -0300

    feat:Added SignUp screen to authentication routes and in the typing file.

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,5 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SignIn} from '../screens/Signin';
+import {SignUp} from '@screens/SignUp';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ export function AuthRoutes() {
   return (
     <Navigator screenOptions={{headerShown: false}}>
       <Screen name="signIn" component={SignIn} />
+      <Screen name="signUp" component={SignUp} />
     </Navigator>
   );
 }
