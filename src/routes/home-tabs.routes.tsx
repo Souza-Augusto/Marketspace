8550fb34873e3e84f3b3cfc4b1c9097f23b531e8
Augusto Souza <souzaaugusto164@gmail.com>
2023-09-15 10:20:33 -0300

    feat:create homeTabs routes.

---
+++ src/routes/home-tabs.routes.tsx
@@ -0,0 +1,65 @@
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs';
+import {Home} from '@screens/Home';
+import {useTheme} from 'styled-components/native';
+import {Ionicons} from '@expo/vector-icons';
+
+type HomeTabsRoutes = {
+  home: undefined;
+};
+
+export type HomeTabsNavigatorRoutesProps =
+  BottomTabNavigationProp<HomeTabsRoutes>;
+
+const {Navigator, Screen} = createBottomTabNavigator<HomeTabsRoutes>();
+
+export function HomeTabsRoutes() {
+  const {COLORS} = useTheme();
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: COLORS.GRAY_200,
+        tabBarInactiveTintColor: COLORS.GRAY_400,
+        tabBarStyle: {
+          backgroundColor: COLORS.GRAY_700,
+          height: 86,
+          paddingBottom: 30,
+          paddingTop: 22,
+        },
+      }}>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name="home-outline" color={color} size={30} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="myAds"
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name="md-pricetag-outline" size={30} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="logOut"
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name="exit-outline" size={30} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
